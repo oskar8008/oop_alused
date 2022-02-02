ed656da996fc6e647ba457ce0f98ed154fb2a417
oskar8008 <oskar-kaarel.kaljund@khk.ee>
2022-02-02 11:16:36 +0200

    dra kasutajad noh update

--- kasutajadvol2/kasutaja.py
+++ kasutajadvol2/kasutaja.py
@@ -6,7 +6,7 @@ kasutaja1.perenimi = "Liiv"
 kasutaja1.vanus = 16
 kasutaja1.gmail = "janre.liiv@voco.ee"
 kasutaja1.kirjelda_kasutaja()
-kasutaja1.tervita_kasutaja()
+kasutaja1.tervita_kasutajat()
 
 kasutaja2 = Kasutajad()
 kasutaja2.eesnimi = "Ax-Sylvester"
@@ -14,7 +14,7 @@ kasutaja2.perenimi = "Hommuk"
 kasutaja2.vanus = 17
 kasutaja2.gmail = "ax-sylvester.hommuk@voco.ee"
 kasutaja2.kirjelda_kasutaja()
-kasutaja2.tervita_kasutaja()
+kasutaja2.tervita_kasutajat()
 
 kasutaja3 = Kasutajad()
 kasutaja3.eesnimi = "Olaf Olev"
@@ -22,4 +22,4 @@ kasutaja3.perenimi = "Alakivi"
 kasutaja3.vanus = 16
 kasutaja3.gmail = "olaf-olev.alakivi@voco.ee"
 kasutaja3.kirjelda_kasutaja()
-kasutaja3.tervita_kasutaja()
+kasutaja3.tervita_kasutajat()
--- kasutajadvol2/kasutajad.py
+++ kasutajadvol2/kasutajad.py
@@ -5,7 +5,7 @@ class Kasutajad():
     vanus = 0
 
     def kirjelda_kasutaja(self):
-        print("kasutaja ees- j aprerkonna nimi on:  {0} {1} ".__format__(self.eesnim,self.perenim))
+        print("kasutaja ees- j aprerkonna nimi on:  {0} {1} ".format(self.eesnim,self.perenim))
         print("kasutaja email onm : {0} ".format(self.email) )
 
     def tervita_kasutajat(self):
