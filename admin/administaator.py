25de9af28538e411a20cf0d8b852944206a36867
oskar8008 <oskar-kaarel.kaljund@khk.ee>
2022-02-09 10:42:32 +0200

    adminnnn

---
+++ admin/administaator.py
@@ -0,0 +1,6 @@
+from admin import Admin
+admin1 = Admin("Olaf Olev", "Alakivi")
+admin1.setandmed(16, "olef-olev.alakivi@voco.ee")
+admin1.kirjelda_kasutaja()
+admin1.privileegid = "Võib muuta kasutajaid"
+admin1.naita_priv()
